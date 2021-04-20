f73e2deadd40df0e2e4cd3c6495bce7752e61822
Ahsan Khan <imkhanahsan@yahoo.com>
2021-04-20 18:15:49 +0500

    Task 4

--- App.js
+++ App.js
@@ -4,28 +4,43 @@ import { TextInput, StyleSheet, Text, View } from 'react-native';
 
 
 export default function App() {
+  
+  const checkPrice = (text) => {
+    if (text > 0 || text == "") {
+      setPrice(text);
+    }
+  }
+  
+  const checkDiscount = (text) => {
+    if (text >= 0 && text <= 100) {
+      setDiscount(text);
+    }
+  }
 
   const [price, setPrice] = useState("");
   const [discount, setDiscount] = useState("");
+  
 
   return (
     <View style={styles.container}>
+      <Text style={styles.header}>Discount Calculator</Text>
       <TextInput
         style={styles.input}
-        onChangeText={setPrice}
+        onChangeText={(text) => checkPrice(text)}
         value={price}
         placeholder="Enter Original Price"
         keyboardType="numeric"
       />
       <TextInput
         style={styles.input}
-        onChangeText={setDiscount}
+        onChangeText={(text) => checkDiscount(text)}
         value={discount}
         placeholder="Enter Discount Percentage"
         keyboardType="numeric"
       />
-      <Text>You Save: {(discount/100) * price}</Text>
-      <Text>Final Price: {price - (discount/100) * price}</Text>
+
+      <Text style={styles.textStyle}>You Save: {parseFloat((discount / 100 * price)).toFixed(2)}</Text>
+      <Text style={styles.textStyle}>Final Price: {parseFloat(price - (discount / 100) * price).toFixed(2)}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -34,15 +49,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#81a4fc',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   input: {
     height: 40,
-    width: 200,
+    width: 220,
+    fontWeight: 'bold',
+    fontSize: 18,
     margin: 12,
-    padding: 3,
-    borderWidth: 1,
+    padding: 5,
+    borderWidth: 2,
+    borderColor: 'white',
+    color: 'black',
+    backgroundColor: '#90affc'
   },
+  header: {
+    fontSize: 50,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'white',
+    marginTop: 40,
+    marginBottom: 10
+  }, 
+  textStyle : {
+    fontSize: 22,
+    color: 'white',
+    fontWeight: 'bold'
+
+  }
 });
